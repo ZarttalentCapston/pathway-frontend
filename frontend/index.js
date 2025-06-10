cb78e7644affe57ff8f5f893004cf27908f4dfeb
EC2 Default User <ec2-user@ip-10-0-0-61.ec2.internal>
2025-06-10 21:17:40 +0000

    Add updates to Docker, deployment workflow, and frontend files

--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// frontend/index.js
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello from frontend!');
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+app.listen(port, () => {
+  console.log(`frontend server running on port ${port}`);
+});
 
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <div>Hello, world! 🚀</div>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
