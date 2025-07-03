18cd2335a31adb50f4f7a1a272c19a573059b510
EC2 Default User <ec2-user@ip-10-0-0-193.ec2.internal>
2025-07-03 21:47:47 +0000

    Deleted all files to reset main branch

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# docker/Dockerfile
-FROM node:18-alpine
-
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . .
-CMD ["npm", "start"]
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => <div>Hello, world! 🚀</div>;
-
-ReactDOM.render(<App />, document.getElementById("root"));
